59bc35d4707d8864449ff5957ba0ca1bc67f9c91
eyauwag <85469843@qq.com>
2014-03-23 14:55:53 +0800

    commit myWeb

---
+++ myWazi/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def hello(request):
+
+	return Http("hello world.")
+	return HttpResponse("hello world.")
+# Create your views here.
